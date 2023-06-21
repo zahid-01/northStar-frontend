176a54d454ad5ffa24764fdab5aff4ebeee35a20
zahid-01 <erzahidkhan101@gmail.com>
2023-06-21 20:22:06 +0530

    Beta V1

--- src/Assets/environment/url.js
+++ src/Assets/environment/url.js
@@ -1,2 +1,2 @@
-// export const URL = "https://north-star-zd0a.onrender.com/";
-export const URL = "http://localhost:5000/";
+export const URL = "https://north-star-zd0a.onrender.com/";
+// export const URL = "http://localhost:5000/";
