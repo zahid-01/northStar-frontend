93eba4655ab70b899e76c05ada3bfb5b254786bf
Basit-Bashir <wbhat56@gmail.com>
2023-07-06 14:43:53 +0530

    links updated

--- src/components/Footer/FooterPart.js
+++ src/components/Footer/FooterPart.js
@@ -10,28 +10,38 @@ const FooterPart = () => {
   return (
     <>
       <div className={classes.FooterCont}>
-        <img alt="Logo" src={logo} className={classes.logo} />
+        <a href="/">
+          <img alt="Logo" src={logo} className={classes.logo} />
+        </a>
         <div className={classes.FooterAdd}>
           <h6>Address</h6>
           <p className={classes.footAdd}>
             Mir Mall Complex opposite District Police Lines Srinagar
           </p>
           <p className={classes.footAdd}>J&K 190001</p>
-          <a href="9797979797">
+          <a className={classes.links} href="tel:9797979797">
             <FontAwesomeIcon icon={faPhone} style={{ fontSize: "12px" }} />
             9797979797
           </a>
-          <a href="northstar@gmail.com">
+          <a className={classes.links} href="mailto:northstar@gmail.com">
             <FontAwesomeIcon icon={faEnvelope} />
             northstar@gmail.com
           </a>
         </div>
         <div className={classes.FooterDet}>
           <h6>Links</h6>
-          <a href="privacy">Privacy Policy</a>
-          <a href="refund">Refund</a>
-          <a href="terms">Terms & Conditions</a>
-          <a href="about">About Us</a>
+          <a className={classes.links} href="privacy">
+            Privacy Policy
+          </a>
+          <a className={classes.links} href="refund">
+            Refund
+          </a>
+          <a className={classes.links} href="terms">
+            Terms & Conditions
+          </a>
+          <a className={classes.links} href="about">
+            About Us
+          </a>
         </div>
       </div>
       <div className={classes.copy}>
