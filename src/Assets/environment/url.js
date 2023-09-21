13931850e91703f70ee317da5e8e6a960a1a8a58
zahid-01 <erzahidkhan101@gmail.com>
2023-09-21 12:19:57 +0530

    Adding items to cart successfully

--- src/Assets/environment/url.js
+++ src/Assets/environment/url.js
@@ -1,4 +1,4 @@
-export const URL = "http://localhost:5000/";
-// export const URL = "https://north-star-zd0a.onrender.com/";
+// export const URL = "http://localhost:5000/";
+export const URL = "https://north-star-zd0a.onrender.com/";
 // export const URL = "https://zany-gray-basket-clam-sari.cyclic.app/";
 export const AWS_URL = "http://nsjk.ap-south-1.elasticbeanstalk.com/";
